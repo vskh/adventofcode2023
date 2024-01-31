4c021178e867f5f5feee533d487efb9ec31a27b8
Vadym S. Khondar <vadym@khondar.name>
2024-01-31 01:04:08 +0000

    [WIP] day3 (runs, wrong answer.)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::io::{BufRead, BufReader};
 use std::ops::RangeInclusive;
 
-fn is_part_number<T: AsRef<str>>(schematic: &Vec<T>, row: usize, range: RangeInclusive<usize>) -> bool {
+fn is_part_number(schematic: &Vec<&str>, row: usize, range: RangeInclusive<usize>) -> bool {
     assert!(row < schematic.len());
 
     // this considers all lines in schematic to be of same length
@@ -19,22 +19,56 @@ fn is_part_number<T: AsRef<str>>(schematic: &Vec<T>, row: usize, range: RangeInc
         if *range.start() == 0 {
             0
         } else {
-            *range.start()
+            *range.start() - 1
         },
         if *range.end() < schematic[row].len() - 1 {
             range.end() + 1
         } else {
-            schematic[row].len()
+            schematic[row].len() - 1
         },
     );
 
+    let num_str = &schematic[row][range.clone()];
+    if !num_str.chars().all(|ch| ch.is_ascii_digit()) {
+        return false;
+    }
+
     for r in r_min..=r_max {
-        if schematic[r][c_min..=c_max].chars().any(|ch| ch != '.') {
+        if schematic[r][c_min..=c_max].chars().any(|ch| ch != '.' && !ch.is_ascii_digit()) {
             return true;
         }
     }
 
     false
+}
+
+fn calc_part_numbers_sum(schematic: &Vec<&str>) -> u64 {
+    let mut part_numbers_sum = 0_u64;
+    for (row, l) in schematic.iter().enumerate() {
+        let mut part_start = -1_i32;
+        for (idx, ch) in l.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if part_start == -1 {
+                    part_start = idx as i32;
+                }
+            } else if part_start != -1 {
+                let part_end = idx - 1;
+                let number =  l[part_start as usize..=part_end].parse::<u64>().unwrap();
+                let is_part = is_part_number(&schematic, row, part_start as usize..=part_end);
+
+                #[cfg(debug_assertions)]
+                println!("Found number '{}' in row {}, is_part = {}", number, row + 1, is_part);
+
+                if is_part {
+                    part_numbers_sum += number;
+                }
+
+                part_start = -1;
+            }
+        }
+    }
+
+    part_numbers_sum
 }
 
 fn main() -> io::Result<()> {
@@ -45,6 +79,13 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         schematic.push(line.unwrap());
     }
+
+    let sch = schematic.iter().map(AsRef::as_ref).collect::<Vec<&str>>();
+
+    // search for part numbers
+    let part_numbers_sum = calc_part_numbers_sum(&sch);
+
+    println!("Sum of all part numbers in schematic is {}", part_numbers_sum);
 
     Ok(())
 }
@@ -67,6 +108,27 @@ mod test {
             .collect::<Vec<&str>>();
 
         assert_eq!(super::is_part_number(&schematic, 0, 5..=7), false);
-        assert_eq!(super::is_part_number(&schematic, 5, 8..=9), false);
+        assert_eq!(super::is_part_number(&schematic, 5, 7..=8), false);
+
+        assert_eq!(super::is_part_number(&schematic, 2, 2..=3), true);
+        assert_eq!(super::is_part_number(&schematic, 7, 6..=8), true);
+    }
+
+    #[test]
+    fn calc_part_numbers_sum_test() {
+        let schematic = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."
+            .split("\n")
+            .collect::<Vec<&str>>();
+
+        assert_eq!(super::calc_part_numbers_sum(&schematic), 4361);
     }
 }
